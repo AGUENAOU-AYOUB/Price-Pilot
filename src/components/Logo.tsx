0742296950373211bd3f8224188d0bda1d35379b
AGUENAOU-AYOUB <aguenaouayoub04@gmail.com>
2025-10-10 00:21:15 +0100

    cleared all TypeScript files

--- src/components/Logo.tsx
+++
@@ -1,9 +0,0 @@
-export function Logo() {
-  return (
-    <img
-      src="//www.azorjewelry.com/cdn/shop/files/473718710_1348387132818131_72892133825066643_n-removebg-preview_1.png?v=1742427290&width=180"
-      alt="Azor Jewelry"
-      className="h-12 w-auto object-contain"
-    />
-  );
-}
